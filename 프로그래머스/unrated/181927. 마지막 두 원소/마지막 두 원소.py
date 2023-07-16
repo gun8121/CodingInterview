2939e548ce73522804853a5d7f28175351d176cc
GunHyun Jeong <gun.devcode@gmail.com>
2023-07-16 20:46:18 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    answer = []
+    if num_list[-1] > num_list[-2]:
+        answer = (num_list[-1] - num_list[-2]) 
+    else:
+        answer = num_list[-1] * 2
+    num_list.append(answer)
+    return num_list
+
+# 마지막 원소가 그전 원소보다 크면 마지막 원소에서 그전 원소를 뺀 값을
+# 마지막 원소가 그전 원소보다 크지 않다면 마지막 원소를 두 배한 값을 추가하여 return

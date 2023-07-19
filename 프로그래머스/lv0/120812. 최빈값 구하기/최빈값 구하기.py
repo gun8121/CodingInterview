70738a491f7cd07e0e11e952663709721af1426f
GunHyun Jeong <gun.devcode@gmail.com>
2023-07-19 21:52:44 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -0,0 +1,7 @@
+def solution(array):
+    while len(array) != 0:
+        for idx, val in enumerate(set(array)):
+            array.remove(val)
+        if idx == 0:
+            return val
+    return -1
